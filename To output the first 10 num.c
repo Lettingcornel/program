bdbfd07baf93d17a34edff33ab16b7ef17ead68d
Lettingcornel <102029806+Lettingcornel@users.noreply.github.com>
2022-05-03 14:41:36 +0300

    Add files via upload

---
+++ To output the first 10 num.c
@@ -0,0 +1,15 @@
+//while loop
+//a program to output the first 10 numbers
+#include<stdio.h>
+int main()
+{
+int n=100;//start
+while(n>0)//stop
+{
+printf("%d\n",n);//print n
+n-=1;//step|
+}
+printf("End of Loop");
+
+return 0;
+}
